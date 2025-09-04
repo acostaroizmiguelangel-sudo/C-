39964dd4c3a475ae9e57b1cb593c34be8b17987b
roizitrus29 <acostaroizmiguelangel@gmail.com>
2025-09-04 02:01:13 -0700

    c++_Arreglos
    Trabajos de C++Miguel Angel Acosta Roiz IS 201

---
+++ ARREGLO1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+    cout << "Hola" << endl;
+
+     arreglo[] = { 2, 3, 4}; 
+
+
+    // Para asignar un nuevo valor al indice 0.
+    arreglo[0] = 1;
+
+    // Para imprimir un valor del arreglo.
+    cout << arreglo[2] << endl;
+    cout << arreglo[0] << endl;
+
+    return 0; 
+}
---
+++ ARREGLO2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    
+    string nombres[] = {"Maikolino"};
+    int edades[] = {18};
+    string ubicaciones[] = {"Los Mochis"};
+
+    cout << "Nombre: " << nombres[0] << endl;
+    cout << "Edad: " << edades[0] << endl;
+    cout << "Ubicacion: " << ubicaciones[0] << endl;
+
+    return 0;
+}
